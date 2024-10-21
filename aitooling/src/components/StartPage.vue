5f247d431288ed1a24772b22241e038fd48645b7
saebuabu <a.saebu@kw1c.nl>
2024-10-21 22:17:27 +0200

    start versie van een nieuw project

---
+++ aitooling/src/components/StartPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Start page</h1>
+  <p>{{ msg }}</p>
+</template>
+
+<script>
+export default {
+  name: 'StartPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+}
+</style>
