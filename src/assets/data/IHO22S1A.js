20f10c09aae02d4de8aae050a53242d3068c733d
saebuabu <a.saebu@kw1c.nl>
2024-10-29 23:15:55 +0100

    Prompting in AIPage met een Google API KEY

--- src/assets/data/IHO22S1A.js
+++ src/assets/data/IHO22S1A.js
@@ -1,6 +1,5 @@
-const IHO22S1A = `
-;1;367913;;;;;Abdullah Al-Anesi
-;2;360640;;;;;Joris van Beers0
+const IHO22S1A = `;1;367913;;;;;Abdullah Al-Anesi
+;2;360640;;;;;Joris van Beers
 ;3;373761;;;;;Rob van den Biggelaar
 ;4;330083;;;;;Robin Boersma
 ;5;372936;;;;;Alyssia Gülük
@@ -11,14 +10,14 @@ const IHO22S1A = `
 ;10;342229;;;;;Ian Pipe
 ;11;375520;;;;;Jacques Prinsen
 ;12;366671;;;;;Noah Schrijver
-;13;376625;;;;;Duc Tan Ta
+;13;376625;;;;;Duc-Tan Ta
 ;14;377571;;;;;Stef Tax
 ;15;376444;;;;;Marijn van Vugt
 ;16;363822;;;;;Angelo Weesenaar
-;17;375871;;;;;Job Zoutewelle
-`;
+;17;375871;;;;;Job Zoutewelle`;
 
 //namenLijst is een array van namen die op iedere regel van de csv staan in de 8 ste kolom, gescheiden door ;
-const namen = IHO22S1A.split('\n').map(line => line.split(';')[7]);
+const namen = IHO22S1A.split('\n').map(line => line.split(';')[7].split(' ')[0]);
 
 export default namen; 
+
--- src/components/AiPage.vue
+++ src/components/AiPage.vue
@@ -3,23 +3,32 @@
         <h1>AI Pagina test 1</h1>
         <input v-model="prompt" placeholder="Voer uw prompt in" />
         <button @click="getAnswer">Verstuur</button>
-        <div v-if="answer">
-            <h2>Antwoord:</h2>
-            <p>{{ answer }}</p>
+        <div v-for="item in chatsession" :key="item.id">
+            <pre>{{ item.prompt }}: {{ item.answer }}</pre>
         </div>
     </div>
 </template>
 <script>
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.VUE_APP_GOOGLE_AP_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
 export default {
     data() {
         return {
             prompt: '',
-            answer: ''
+            answer: '',
+            chatsession: []
         };
     },
     methods: {
-        getAnswer() {
-            this.answer = `Dit is een voorbeeldantwoord voor de prompt: ${this.prompt}`;
+        async getAnswer() {
+
+            const result = await model.generateContent(this.prompt);
+            this.answer = `${result.response.text()}`;
+            this.chatsession.push({ id: this.chatsession.length, prompt: this.prompt, answer: this.answer });
+            this.prompt = '';
         }
     }
 };
@@ -28,5 +37,6 @@ export default {
 <style scoped>
 input {
     margin-right: 10px;
+    width: 50%;
 }
 </style>
